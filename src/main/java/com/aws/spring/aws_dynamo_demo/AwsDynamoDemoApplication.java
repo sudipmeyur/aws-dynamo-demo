9712c4aa57bbf7a4bc071ed9622e89040292c95c
sudipmeyur <sudip.meyur@ltimindtree.com>
2024-06-26 16:46:03 +0530

    init commit

---
+++ src/main/java/com/aws/spring/aws_dynamo_demo/AwsDynamoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aws.spring.aws_dynamo_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDynamoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDynamoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/spring/aws_dynamo_demo/AwsDynamoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.spring.aws_dynamo_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDynamoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
